6ab3a32f8159a20d3683a96590af462b0abef078
2xhamzeh <hamzehx2@outlook.com>
2024-08-08 15:14:45 +0200

    added better validation for name, email, password / create,login,update user

--- src/features/users/user.validator.ts
+++ src/features/users/user.validator.ts
@@ -1,36 +1,32 @@
 import { body } from "express-validator";
 
-export const createUserValidator = [
-  body("email").trim().isEmail().withMessage("Invalid email"),
+const emailValidator = () =>
+  body("email").trim().isEmail().withMessage("Invalid email").normalizeEmail();
+const passwordValidator = () =>
   body("password")
     .trim()
-    .isLength({ min: 6 })
-    .withMessage("Password must be at least 6 characters")
-    .escape(),
-  body("name").trim().isString().withMessage("Name must be a string").escape(),
-];
-
-export const updateUserValidator = [
-  body("email").optional().trim().isEmail().withMessage("Invalid email"),
-  body("password")
-    .optional()
-    .trim()
-    .isLength({ min: 6 })
-    .withMessage("Password must be at least 6 characters")
-    .escape(),
+    .isLength({ min: 6, max: 20 })
+    .withMessage("Password must be between 6 and 20 characters")
+    .escape();
+const nameValidator = () =>
   body("name")
-    .optional()
     .trim()
     .isString()
     .withMessage("Name must be a string")
-    .escape(),
+    .isLength({ min: 1, max: 20 })
+    .withMessage("Name must be between 1 and 20 characters")
+    .escape();
+
+export const createUserValidator = [
+  emailValidator(),
+  passwordValidator(),
+  nameValidator(),
 ];
 
-export const loginValidator = [
-  body("email").trim().isEmail().withMessage("Invalid email"),
-  body("password")
-    .trim()
-    .isLength({ min: 6 })
-    .withMessage("Password must be at least 6 characters")
-    .escape(),
+export const updateUserValidator = [
+  emailValidator().optional(),
+  passwordValidator().optional(),
+  nameValidator().optional(),
 ];
+
+export const loginValidator = [emailValidator(), passwordValidator()];
